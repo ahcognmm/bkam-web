b6ad63c314b426b8db6a0159593dcbb7445b683b
ahcognmm <laohac1507@gmail.com>
2018-10-25 22:05:56 +0700

    cmt something

--- router/Admin.js
+++ router/Admin.js
@@ -5,7 +5,5 @@ import * as Admin from '../Controller/AdminController'
 
 module.exports = function (app: any) {
 
-    app.post('/admin-bkam/login',)
-    app.get('/admin-bkam/index', Admin.is)
-    app.get()
+    // app.post('/admin-bkam/login',)
 }
